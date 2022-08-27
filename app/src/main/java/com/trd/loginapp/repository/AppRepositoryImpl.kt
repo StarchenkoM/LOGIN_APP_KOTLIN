ccba6ba8b72309c397c5c97a82336615383044b7
Maksym Starchenko <maksym_starchenko@epam.com>
2022-08-27 19:22:53 +0300

    repository refactoring

--- app/src/main/java/com/trd/loginapp/repository/AppRepositoryImpl.kt
+++ app/src/main/java/com/trd/loginapp/repository/AppRepositoryImpl.kt
@@ -3,10 +3,16 @@ package com.trd.loginapp.repository
 import android.util.Log
 import com.trd.loginapp.api.LoginApi
 import com.trd.loginapp.api.LoginData
+import com.trd.loginapp.api.LoginDataApiResponse
 import com.trd.loginapp.database.DataMapper
 import com.trd.loginapp.database.UserDao
 import com.trd.loginapp.states.LoadingState
+import com.trd.loginapp.states.LoadingState.LoadingError
+import com.trd.loginapp.states.LoadingState.LoadingSuccess
 import com.trd.loginapp.states.LoginState
+import com.trd.loginapp.states.LoginState.LoginError
+import com.trd.loginapp.states.LoginState.LoginSuccess
+import retrofit2.Response
 import javax.inject.Inject
 
 class AppRepositoryImpl @Inject constructor(
@@ -18,14 +24,22 @@ class AppRepositoryImpl @Inject constructor(
         val response = loginApi.postLoginData(loginData)
         Log.i("***888", "postLoginData: responce = $response")
         Log.i("***888", "postLoginData: responce.body() = ${response.body()}")
+        return provideLoginResult(response)
+    }
+
+    private fun provideLoginResult(response: Response<LoginDataApiResponse>): LoginState {
         return if (response.isSuccessful) {
-            val body = response.body()
-            val dbItem = body?.let { dataMapper.mapApiItemToDBEntity(it) }
-            dbItem?.let { userDao.insertUserToDb(it) }
-            LoginState.LoginSuccess
+            val savingResult = saveResponseToDB(response)
+            if (savingResult > 0) LoginSuccess else LoginError
         } else {
-            LoginState.LoginError
+            LoginError
         }
+    }
+
+    private fun saveResponseToDB(response: Response<LoginDataApiResponse>): Long {
+        val dbItem = response.body()?.let { dataMapper.mapApiItemToDBEntity(it) }
+        val savingResult = dbItem?.let { userDao.insertUserToDb(it) } ?: -1
+        return savingResult
     }
 
     override suspend fun getUserById(phoneNumber: String): LoadingState {
@@ -33,9 +47,9 @@ class AppRepositoryImpl @Inject constructor(
         val dbItem = userDao.getUserById(phoneNumber)
         return if (dbItem != null) {
             val userItem = dataMapper.mapDBItemToUserItem(dbItem)
-            LoadingState.LoadingSuccess(userItem)
+            LoadingSuccess(userItem)
         } else {
-            LoadingState.LoadingError
+            LoadingError
         }
     }
 
